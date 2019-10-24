c7321b99d87e5a2e3b2e447accb530b4fa29e48b
yusaku916 <yy.yusaku916@ezweb.ne.jp>
2019-10-24 23:32:10 +0900

    Automatic_update_function_add2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -49,24 +49,6 @@ $(document).on('turbolinks:load', function() {
     };
     return html;
   };
-
-  //   var html = `<div class="chat" data-id="${message.id}">
-  //                 <div class="chat__upper-chat">
-  //                   <p class="chat__upper-chat__user">
-  //                     ${message.user_name}
-  //                   </p>
-  //                   <p class="chat__upper-chat__date">
-  //                     ${message.date}
-  //                   </p>
-  //                 </div>
-  //                 <p class="chat__message">
-  //                   ${content}
-  //                   </div>
-  //                   ${img}
-  //                 </p>
-  //               </div>`
-  // return html;
-  //}
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var message = new FormData(this);
@@ -80,7 +62,6 @@ $(document).on('turbolinks:load', function() {
       contentType: false
     })
     .done(function(data){
-      // console.log("a")
       var html = buildHTML(data);
       $('.main__right__chats').append(html);
       $('form')[0].reset();
@@ -98,15 +79,11 @@ $(document).on('turbolinks:load', function() {
     })
   })
     var reloadMessages = function() {
-      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
       last_message_id = $('.chat:last').data("id")
       $.ajax({
-        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
         url: 'api/messages',
-        //ルーティングで設定した通りhttpメソッドをgetに指定
         type: 'get',
         dataType: 'json',
-        //dataオプションでリクエストに値を含める
         data: {id: last_message_id}
       })
       .done(function(messages) {
