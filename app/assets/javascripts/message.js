0aa024ef4a3922072d83b20267639d787eab4dab
yusaku916 <yy.yusaku916@ezweb.ne.jp>
2019-10-26 20:19:17 +0900

    y

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -22,7 +22,8 @@ $(document).on('turbolinks:load', function() {
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var message = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
+    console.log(url)
     $.ajax({
       url: url,
       type: "POST",
@@ -48,32 +49,28 @@ $(document).on('turbolinks:load', function() {
       $('.send').prop('disabled', false);
     })
   })
-    var reloadMessages = function() {
-      last_message_id = $('.chat:last').data("id")
-      $.ajax({
-        url: 'api/messages',
-        type: 'get',
-        dataType: 'json',
-        data: {id: last_message_id}
-      })
-      .done(function(messages) {
-        var insertHTML = '';
-        messages.forEach(function(message){
-          insertHTML = buildHTML(message);
-          $('.main__right__chats').append(insertHTML);
-        })
-        var target = $('.chat').last();
-        var position = target.offset().top + $('.main__right__chats').scrollTop();
-        $('.main__right__chats').animate({
-        scrollTop: position
-      }, 300, 'swing');
-      })
-      .fail(function() {
-        alert('自動更新に失敗しました');
-      });
-    }
-    
-  if (window.location.href.match(/\/groups\/\d+\/messages/)){
-  setInterval(reloadMessages, 5000);
-  };
+  // if (window.location.href.match(/\/groups\/\d+\/messages/)){
+  //   var reloadMessages = function() {
+  //     last_message_id = $('.chat:last').data("id")
+  //     $.ajax({
+  //       url: 'api/messages',
+  //       type: 'get',
+  //       dataType: 'json',
+  //       data: {id: last_message_id}
+  //     })
+  //     .done(function(messages) {
+  //       var insertHTML = '';
+  //       messages.forEach(function(message){
+  //         insertHTML = buildHTML(message);
+  //         $('.main__right__chats').append(insertHTML);
+  //       })
+  //       var target = $('.chat').last();
+  //       $('.main__right__chats').animate({scrollTop: $(".main__right__chats")[0].scrollHeight}, "fast");
+  //     })
+  //     .fail(function() {
+  //       alert('自動更新に失敗しました');
+  //     });
+  //   }
+  // //setInterval(reloadMessages, 5000);
+  // };
 });
